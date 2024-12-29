b0dabb6bf1522167098e6fa7806c87ecdf7bf324
Burke Cates <burke.cates@gmail.com>
2024-12-29 00:55:44 -0500

    shell: turn on direnv. its pretty magic

--- shell.nix
+++ shell.nix
@@ -9,6 +9,16 @@
     zsh-completions
   ];
 
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+    config = {
+      global.hide_env_diff = true;
+    };
+  };
+
   programs.bash.enable = true;
 
   programs.zsh = {
