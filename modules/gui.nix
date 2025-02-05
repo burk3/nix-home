f68083b6bd02168a45e4ae5a8f1c5cf27bec45ae
Burke Cates <burke.cates@gmail.com>
2025-02-05 01:17:05 -0500

    trying out zed editor

--- modules/gui.nix
+++ modules/gui.nix
@@ -1,4 +1,4 @@
-{ pkgs, ghostty, config, ... }:
+{ pkgs, lib, config, ... }:
 {
   nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
@@ -45,7 +45,63 @@
     };
   };
 
+  programs.zed-editor = {
+      enable = true;
+      extensions = [
+        "nix"
+        "toml"
+        "make"
+        "git-firefly"
+        "dockerfile"
+        "terraform"
+        "docker-compose"
+      ];
+      userSettings = {
+        assistant = {
+          enabled = true;
+          version = "2";
+          default_model = {
+            provider = "zed.dev";
+            model = "claude-3-5-sonnet-latest";
+          };
+          inline_alternatives = [
+            {
+              provider = "copilot_chat";
+              model = "gpt-4o";
+            }
+          ];
+        };
+        node = {
+          path = lib.getExe pkgs.nodejs;
+          npm_path = lib.getExe' pkgs.nodejs "npm";
+        };
 
+        hour_format = "hour24";
+        auto_update = false;
+        terminal = {
+          alternate_scroll = "off";
+          blinking = "off";
+          dock = "bottom";
+          env = {
+            TERM = "alacritty";
+            EDITOR = "${lib.getExe pkgs.zed-editor} --wait";
+          };
+          font_family = "Berkeley Mono";
+          font_featues = null;
+          line_height = "comfortable";
+          button = false;
+          shell = "system";
+          toolbar.title = true;
+          working_directory = "current_project_directory";
+        };
+        lsp = {
+          rust-analyzer.binary.path_lookup = true;
+          nix.binary.path_lookup = true;
+          gopls.binary.path_lookup = true;
+        };
+        vim_mode = true;
+      };
+    };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
